027c5eef27934978d9ce586368f6fa3970edeabf
Ryan Chipman <ryan@ryanchipman.com>
2018-10-16 06:00:48 -0400

    Derive debug for cst structs

--- src/cst.rs
+++ src/cst.rs
@@ -1,4 +1,5 @@
 
+#[derive(Debug)]
 pub enum Expr {
     Binary(Box<Expr>, BinaryOp, Box<Expr>),
     Unary(UnaryOp, Box<Expr>),
@@ -9,6 +10,7 @@ pub enum Expr {
     Case,
 }
 
+#[derive(Debug)]
 pub enum UnaryOp {
     Plus,
     Minus,
@@ -16,6 +18,7 @@ pub enum UnaryOp {
     Not,
 }
 
+#[derive(Debug)]
 pub enum BinaryOp {
     And,
     Or,
@@ -37,6 +40,7 @@ pub enum BinaryOp {
     IsNot,
 }
 
+#[derive(Debug)]
 pub enum Literal {
     Boolean(bool),
     Date(),
@@ -45,70 +49,83 @@ pub enum Literal {
     Null,
 }
 
+#[derive(Debug)]
 pub enum Number {
     Integer(i64),
     Float(f64),
 }
 
+#[derive(Debug)]
 pub struct Limit {
     pub limit: Expr,
     pub offset: Option<Expr>,
 }
 
+#[derive(Debug)]
 pub enum Statement {
     Select(Select),
 }
 
+#[derive(Debug)]
 pub struct Select {
     pub projects: Projects,
     pub limit: Option<Limit>,
     pub table: Option<TableExpr>,
 }
 
+#[derive(Debug)]
 pub enum TableExpr {
     List(Vec<Table>),
     Table(Table),
 }
 
+#[derive(Debug)]
 pub enum Table {
     Name(TableName),
     Derived(DerivedTable),
     Join(Join),
 }
 
+#[derive(Debug)]
 pub struct Join {
     pub left: Box<Table>,
     pub right: Box<Table>,
     pub predicate: JoinPredicate,
 }
 
+#[derive(Debug)]
 pub enum JoinPredicate {
     On(Expr),
     Using(Vec<ColumnName>),
 }
 
+#[derive(Debug)]
 pub struct ColumnName {
     pub name: String,
     pub db: Option<String>,
     pub qualifier: Option<String>,
 }
 
+#[derive(Debug)]
 pub struct TableName {
     pub name: String,
     pub qualifier: String,
     pub alias: Option<String>,
 }
 
+#[derive(Debug)]
 pub struct DerivedTable {
     pub subquery: Box<Select>,
     pub alias: String,
 }
 
+#[derive(Debug)]
 pub enum Projects {
     Star,
     NonStar(Vec<AliasedColumn>),
 }
 
+#[derive(Debug)]
 pub struct AliasedColumn {
     pub expr: Expr,
     pub alias: Option<String>,
